c9d703eb6e194d75cb3812bd053843f18f17b327
Manshi Mahato <manshi.mahato@planview.com>
2024-08-28 12:40:28 +0530

    Initial commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Calculator class
+            Calculator calculator = new Calculator();
+
+            // Perform addition using the Add method
+            int result = calculator.Add(5, 7);
+
+            // Display the result
+            Console.WriteLine($"The result of adding 5 and 7 is: {result}"); // Output: The result of adding 5 and 7 is: 12
+        }
+    }
+}
+
